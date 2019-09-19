3ab941f9211b358a66c3aacfa39f15319793cfc8
gustavwiberg <gustav.wiberg@ntig.se>
2019-09-19 23:00:19 +0200

    bas frukt

---
+++ partytalt.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Git out of here</title>
+            <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="">
+</head>
+<body>
+<img src="bilder/espresso.jpg">>>
+    <?php
+try {$frukt1 = true;$frukt2 = true;$banan = 1;$banan_ar_frukt = $frukt1;
+$Äpple = 2;$apple_ar_frukt = $frukt2;
+        if ($banan_ar_frukt == true && $banan_ar_frukt == true)
+if ($banan + 1 == $Äpple && $Äpple == $banan +1 ) echo 'suveränt';
+$banan = 100000000000000000000000000000000000;
+}catch (Exception $e) {
+    echo 'Caught exception: ',  $e->getMessage(), "\n";
+    
+}
+?>
+    </body>
+</html>
+<?php
+error_log('nu har vi kört denna X antal gånger',3,'error.log');
+/*
+Git out of here!
+Syftet med denna hemsida är att visa om en banan eller äpple är en frukt
+(Kan det bli bättre än ovan????)
+*/
+?>
